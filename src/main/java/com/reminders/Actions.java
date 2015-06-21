3947e3c2c8c065fda43687ab0ad33c952aa064d9
silvian <silvian.dragan@gmail.com>
2015-06-21 20:08:47 +0100

    added main index

---
+++ src/main/java/com/reminders/Actions.java
@@ -0,0 +1,20 @@
+package com.reminders;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by silvian on 21/06/15.
+ */
+@Controller
+public class Actions {
+
+    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+    public String displayIndex(Model model) {
+
+        return "index";
+    }
+
+}
